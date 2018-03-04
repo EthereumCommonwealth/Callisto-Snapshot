520537169e1df0865aa29e7ffde17cf1ac3c9378
Yohan Graterol <yohangraterol92@gmail.com>
2018-03-04 16:15:19 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const net = require('net')
+const fs = require('fs')
+const Web3 = require('web3')
+
+const snapshotBlock = "32862" // ETC block
+
+const ipcPath = process.env["HOME"] + "/.local/share/io.parity.ethereum/jsonrpc.ipc";
+
+let web3 = new Web3();
+web3.setProvider(new web3.providers.IpcProvider(ipcPath, net));
+
+let snapshot = {}
+
+const getAccounts = (accountOffset, callback) => {
+  return web3.parity.listAccounts(
+    5, accountOffset, web3.toHex(snapshotBlock), function (err, result) {
+      if (err)
+        console.fatal(err)
+      callback(result)
+    }
+  )
+}
+
+const getBalance = (account) => {
+  web3.eth.getBalance(account, web3.toHex(snapshotBlock), (err, result) => {
+    if (err)
+      console.fatal(err)
+    snapshot[account] = result.toString(10)
+  })
+}
+
+const writeJSONFile = () => {
+  fs.writeFileSync('snapshot.json', JSON.stringify(snapshot));
+}
+
+const getBalances = (accounts) => {
+  if (accounts.length === 0) {
+    writeJSONFile()
+    process.exit()
+  }
+
+  accounts.forEach((account, idx) => {
+    getBalance(account)
+    if (idx === accounts.length - 1) {
+      console.log("Last address: ", account)
+      makeSnapshot(account)
+    }
+  })
+}
+
+const makeSnapshot = (accountOffset) => {
+  getAccounts(accountOffset, getBalances)
+}
+
+makeSnapshot(null)
