c5a432df40ad66df8ed9ccf113fc7d09ab83c10e
Yohan Graterol <yohangraterol92@gmail.com>
2018-03-19 13:46:02 -0500

    Fixed send_balance script

--- send_balance.py
+++ send_balance.py
@@ -1,9 +1,15 @@
+import os
 import time
 import pandas as pd
 
-from web3 import Web3, IPCProvider
+from pathlib import Path
 
-web3 = Web3(IPCProvider)
+from web3 import Web3, IPCProvider
+from pathlib import Path
+
+ipc_file = os.path.join(Path.home(), '.callisto/testnet/geth.ipc')
+
+web3 = Web3(IPCProvider(ipc_file))
 
 addresses_df = pd.read_csv(
     'snapshot/snapshot_0001.txt',
@@ -13,11 +19,13 @@ addresses_df = pd.read_csv(
 total_balance = 0
 transactions_count = 0
 
+
 with open('geth_genesis.json', 'a') as genesis_file:
+    amount_transactions = len(addresses_df.itertuples())
     for row in addresses_df.itertuples():
         transactions_count += 1
         web3.eth.sendTransaction({
-            'from': web3.eth.coinbase,
+            'from': '0x183394f52b2c8c034835edba3bcececa6f60b5a8',
             'to': row.address,
             'value': int(row.balance),
             'gas': 21000,
@@ -25,7 +33,9 @@ with open('geth_genesis.json', 'a') as genesis_file:
         })
         total_balance += int(row.balance)
 
-        if transactions_count == 1000:
-            time.sleep(60)
+        if transactions_count == 8000:
+            print('Last Transaction Address: {}'.format(row.address))
+            time.sleep(20)
+            transaction_count = 0
 
 print(total_balance)
